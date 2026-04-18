8f9e16d72e5aec46d123bc49b7536a077d7c5749
Daniel Pederzini <pederzinidaniel@hotmail.com>
2026-04-18 00:34:39 -0300

    Add softmax layer

--- notebooks/model/__init__.py
+++ notebooks/model/__init__.py
@@ -1,8 +1,9 @@
 from .layer import Layer
 from .relu_layer import ReluLayer
 from .sigmoid_layer import SigmoidLayer
+from .softmax_layer import SoftmaxLayer
 from .network import Network
 from .conv_layer import ConvLayer
 from .max_pool_layer import MaxPoolLayer
 
-__all__ = ["Layer", "ReluLayer", "SigmoidLayer", "Network", "ConvLayer", "MaxPoolLayer"]
+__all__ = ["Layer", "ReluLayer", "SigmoidLayer", "SoftmaxLayer", "Network", "ConvLayer", "MaxPoolLayer"]
--- notebooks/model/network.py
+++ notebooks/model/network.py
@@ -3,6 +3,7 @@ import cupy as cp
 from .layer import Layer
 from .relu_layer import ReluLayer
 from .sigmoid_layer import SigmoidLayer
+from .softmax_layer import SoftmaxLayer
 from .conv_layer import ConvLayer
 from .max_pool_layer import MaxPoolLayer
 
@@ -20,6 +21,7 @@ class Network:
         "MaxPool": MaxPoolLayer,
         "ReLU": ReluLayer,
         "Sigmoid": SigmoidLayer,
+        "Softmax": SoftmaxLayer,
     }
     
     def __init__(self, layer_definitions: list[dict]) -> None:
